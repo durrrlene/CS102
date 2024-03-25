6e8010e8353f78378812b73082fb0db62363d581
durrrlene <darlene7703@gmail.com>
2024-03-25 12:13:40 +0800

    abbreviated months

--- DataCleaning(3000)/dataCleaning.R
+++ DataCleaning(3000)/dataCleaning.R
@@ -1,5 +1,6 @@
 library(dplyr)
 library(tidyverse)
+library(lubridate)
 
 deets <- read.csv("C:/Users/steve/Documents/CS102/DataCleaning(3000)/3000MergedReviews.csv")
 
@@ -17,18 +18,28 @@ is.na(tail(deets))
 sum(is.na(deets))
 
 
+
+
+
 #change format of date
 cformat_dates <- dmy(deets$Date)
-posix_dates <- as.POSIXct(cformat_dates, format = "%m-%d-%Y", tz = "")
+cformat_dates <- as.POSIXct(cformat_dates, format = "%b-%d-%Y", tz = "")
+
+#making the month into its abbreviated name
+cformat_dates_char <- format(cformat_dates, "%b-%d-%Y")
+
 
 #removing the UTC and PST(timezone information)
-formatted_dates <- format(posix_dates, "%m-%d-%Y")
+formatted_dates <- gsub("UTC|PST", "", cformat_dates_char)
+
 formatted_dates
 
 #removing the UTC and PST allows me to correct the format of the date to M-D-Y,
 #while it still has I encounter problem converting those to new format.
 
 
+
+#clean the reviews to lower cases
 
 
 
